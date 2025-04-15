8d8a23ff4e90d03fb24496776b667a04c37e19e4
ChristineCGomez <christine.gomez@smc.pshs.edu.ph>
2025-04-15 22:34:31 +0800

    Update script 8.js

--- script 8.js
+++ script 8.js
@@ -2,19 +2,25 @@ function generateTable() {
     let start = parseInt(document.getElementById("startNum").value);
     let end = parseInt(document.getElementById("endNum").value);
 
-    if (isNaN(start) || isNaN(end) || start < 2 || end > 10 || start > end) {
-        alert("Please enter numbers between 2 and 10");
-        return;
-    }
+    if (startNum> 2 && startNum < 10 && endNum> 2 && endNum < 10) {
 
-    let output = "<table border='1' cellpadding='10'>";
-    for (let i = 1; i <= 10; i++) {
-        output += "<tr>";
-        for (let j = start; j <= end; j++) {
-            output += `<td>${j} x ${i} = ${j * i}</td>`;
+        result += "<table>";
+
+        for (let m = 1; m <= startNum; m++) {
+            result += "<tr>";
+            for (let n = 1; n <= endNum; n++) {
+                result += "<td>" + (m * n) + "</td>";
+            }
+            result += "</tr>";
         }
-        output += "</tr>";
+
+        result += "</table>";
+        document.getElementById("output").innerHTML = result;
+        
+    } else {
+
+        alert("Please enter numbers between 2 and 10");
+
     }
-    output += "</table>";
-    document.getElementById("output").innerHTML = output;
+    
 }
